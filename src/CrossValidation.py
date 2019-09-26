1fef241f2a826a6be2b585c714ed98dcac05df44
Ian Benlolo <ian.benlolo@mail.mcgill.ca>
2019-09-26 18:17:31 -0400

    Made logistic take in learning parameters in run function and not constructor

--- src/CrossValidation.py
+++ src/CrossValidation.py
@@ -13,7 +13,7 @@ import src.eval
 # data - the data set
 # model - model to train
 # fold - number of folds
-def CrossValidation(data, model, fold):
+def CrossValidation(data, model, fold, params = None):
     folds = np.array_split(data, fold, axis=0)  # we separate the data set into k different sub lists
     accuracy = 0
     for i in range(fold):
@@ -21,7 +21,13 @@ def CrossValidation(data, model, fold):
         for j in range(fold):
             if j != i:
                 trainingset = np.concatenate((trainingset, folds[j]))  # we create our training set by adding everysub lists except the ith one used for validation
-        model.fit(trainingset[:,:-1], trainingset[:,-1])  # train the model with the training set
+        if params is not None:
+            assert(len(params) == 3, 'params should have iterations, lr, lr_func')
+        else:
+            params = []
+
+        model.fit(trainingset[:,:-1], trainingset[:,-1], *params)  # train the model with the training set
+
         predictions = model.predict(folds[i][:,:-1])  # predict using validation set
         accuracy += src.eval.evaluate_acc(folds[i][:,-1], predictions)
     return accuracy / fold
--- src/Experiments.py
+++ src/Experiments.py
@@ -3,6 +3,8 @@
 # Author: Thomas Racine
 # Purpose: Run Experiments on our different models
 ######################################################
+import sys
+sys.path.append('/Users/ianbenlolo/Desktop/Mcgill/comp_551/ML-Project1/')
 import src.load_files
 import src.Linear_discriminant_analysis
 import src.logistic_regression
@@ -14,9 +16,12 @@ wines, wine_headers = src.load_files.load_wine()
 cancer = src.load_files.load_cancer()
 
 model_lda_wine1 = src.Linear_discriminant_analysis.LDA(0,0)
-model_linear_regression_wine1 = src.logistic_regression.Logistic(wines.shape[1], 1000, 0.004, lambda x, y: x)
-model_linear_regression_wine2 = src.logistic_regression.Logistic(wines.shape[1], 1000, 0.004, lambda x, y: x/np.round(np.log10(y),0) if y > 10 else x)
 
+model_linear_regression_wine1 = src.logistic_regression.Logistic(wines.shape[1])
+params1 = [1000, 0.004, lambda x, y: x]
+
+model_linear_regression_wine2 = src.logistic_regression.Logistic(wines.shape[1])
+params2 = [1000, 0.004,  lambda x, y: x/np.round(np.log10(y),0) if y > 10 else x]
 
 print('*******Runtime and accuracy of our models*******')
 print('Linear Discriminant Analysis')
@@ -27,6 +32,6 @@ print(acc)
 
 print('\nLinear regression: 1000 steps, learning rate 0.004 ')
 start_time = time.time()
-acc = src.CrossValidation.CrossValidation(wines.copy(), model_linear_regression_wine1, 5)
+acc = src.CrossValidation.CrossValidation(wines.copy(), model_linear_regression_wine1, 5, params1)
 print(time.time() - start_time)
 print(acc)
