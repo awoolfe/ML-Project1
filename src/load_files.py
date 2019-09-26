Fixed normalization for cancer data

src/load_files.py
@@ -18,7 +18,7 @@ def load_wine(wine_data_path ='../data/winequality-red.csv', preprocess = 1):
             else:
                 i[-1] = 0
     wines[:,:-1] = normalize(np.copy(wines[:,:-1]))
-    return normalize(wines), wine_headers
+    return wines, wine_headers
 
 
 def load_cancer(breast_cancer_data_path = '../data/breast-cancer-wisconsin.data' , preprocess = 1):
@@ -45,7 +45,9 @@ def load_cancer(breast_cancer_data_path = '../data/breast-cancer-wisconsin.data'
                 print('Something weird. Check cancer data.',i)
     # return list into numpy array
     cancer = np.asarray(cancer_good)
-    cancer[:,:-1] = normalize(np.copy(cancer[:,:-1]))
+    y = cancer[:,-1]
+    cancer = normalize(cancer)
+ #   cancer = np.concatenate((cancer, y[np.newaxis:]), axis=1)
 
     return cancer, cancer_headers
 